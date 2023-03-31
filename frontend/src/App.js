06cdb50ccabd3ba7667a8a3bd1fd65d457678a71
0xMilica <0xmilica@gmail.com>
2023-03-31 04:05:38 +0200

    Created wallet connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import logo from './zksynclogo.png';
 import './App.css';
+import "@rainbow-me/rainbowkit/styles.css";
+import { connectorsForWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { injectedWallet } from '@rainbow-me/rainbowkit/wallets';
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { zkSyncTestnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function App() {
+
+  const { chains, provider } = configureChains(
+    [zkSyncTestnet],
+    [publicProvider()]
+  );
+  
+  const connectors = connectorsForWallets([
+    {
+      groupName: 'Recommended',
+      wallets: [
+        injectedWallet({ chains })
+      ],
+    },
+  ]);
+  
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains}>
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <b>Number Guessing game deployed on zkSync ERA</b>
+                <ConnectButton/>
+              </header>
+              <div className="body">
+                
+              </div>
+            </div>
+          </RainbowKitProvider>
+        </WagmiConfig>
   );
 }
 
